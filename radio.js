f9c6610773733a1f56fc28a44a7d4efcfc0306b8
ViniciusMarcus <141650235+Marcus0008@users.noreply.github.com>
2023-09-25 10:39:29 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,57 +1,62 @@
 const audio = document.getElementById("audio");
 const playlist = document.getElementById("playlist");
 const playlistItems = playlist.querySelectorAll("li");
-const songName = document.getElementById("song-name");
-const songImage = document.createElement("img");
-songImage.id = "expanded-image";
+const songInfo = document.getElementById("song-info");
+const songTitle = document.getElementById("song-title");
+const songImage = document.getElementById("song-image");
+const prevButton = document.getElementById("prev-button");
+const pauseButton = document.getElementById("pause-button");
+const nextButton = document.getElementById("next-button");
 
 let currentIndex = -1;
+
+function playCurrentSong() {
+    if (currentIndex >= 0 && currentIndex < playlistItems.length) {
+        const currentItem = playlistItems[currentIndex];
+        const src = currentItem.getAttribute("data-src");
+        const name = currentItem.textContent; // Use o texto do item como nome
+        const image = currentItem.getAttribute("data-image");
+        audio.src = src;
+        audio.load();
+        audio.play();
+        songTitle.textContent = name;
+        songImage.src = `images/${image}`;
+        songInfo.style.display = "block";
+    }
+}
+
+function playNextSong() {
+    currentIndex++;
+    if (currentIndex >= playlistItems.length) {
+        currentIndex = 0;
+    }
+    playCurrentSong();
+}
+
+function playPrevSong() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = playlistItems.length - 1;
+    }
+    playCurrentSong();
+}
 
 playlistItems.forEach((item, index) => {
     item.addEventListener("click", function() {
         currentIndex = index;
-        const src = this.getAttribute("data-src");
-        const name = this.textContent;
-        audio.src = src;
-        audio.load();
-        audio.play();
-        songName.textContent = name;
-        songName.style.display = "block";
-        songImage.src = ""; // Remova a imagem atual
-        songImage.style.display = "none"; // Oculta a imagem
+        playCurrentSong();
     });
 });
 
-audio.addEventListener("ended", function() {
-    playNextSong();
-});
+audio.addEventListener("ended", playNextSong);
 
-function playNextSong() {
-    currentIndex++;
-    if (currentIndex < playlistItems.length) {
-        const nextItem = playlistItems[currentIndex];
-        const src = nextItem.getAttribute("data-src");
-        const name = nextItem.textContent;
-        audio.src = src;
-        audio.load();
+prevButton.addEventListener("click", playPrevSong);
+pauseButton.addEventListener("click", () => {
+    if (audio.paused) {
         audio.play();
-        songName.textContent = name;
-        songImage.src = ""; // Remova a imagem atual
-        songImage.style.display = "none"; // Oculta a imagem
     } else {
-        currentIndex = -1;
         audio.pause();
-        songName.style.display = "none"; // Oculta o nome da música
-        songImage.style.display = "none"; // Oculta a imagem
     }
-}
-
-// Adicionar controle para próxima música
-const controls = document.createElement("div");
-controls.id = "controls";
-const nextButton = document.createElement("button");
-nextButton.id = "next-button";
-nextButton.textContent = "Próxima Música";
+});
 nextButton.addEventListener("click", playNextSong);
-controls.appendChild(nextButton);
-songName.parentNode.appendChild(controls);
+
